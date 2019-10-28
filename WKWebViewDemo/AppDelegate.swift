9c29a08bedd55629f6095f96f61c73cc0f945b3b
Heaven Chou <heaven.chou@gmail.com>
2019-10-28 23:23:18 +0800

    Initial Commit

---
+++ WKWebViewDemo/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  WKWebViewDemo
+//
+//  Created by Heaven Chou on 2019/10/28.
+//  Copyright © 2019 CBETA. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
