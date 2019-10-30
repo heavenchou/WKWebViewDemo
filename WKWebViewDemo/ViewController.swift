8798cf7647c80414559a185c2f8059965f4b8f93
Heaven Chou <heaven.chou@gmail.com>
2019-10-30 21:06:29 +0800

    WebView 加上約束，並重構程式碼。

--- WKWebViewDemo/ViewController.swift
+++ WKWebViewDemo/ViewController.swift
@@ -13,20 +13,14 @@ class ViewController: NSViewController, WKUIDelegate, WKNavigationDelegate {
     
     let webView = WKWebView()
     
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
         
-        self.webView.uiDelegate = self
-        self.webView.navigationDelegate = self
-        
-        webView.frame = CGRect(x:0,y:0,width:400,height: 270)
-        view.addSubview(webView)
-                
-        let myURL=URL(string: "https://www.apple.com")
-        let myRequest = URLRequest(url: myURL!)
-        webView.load(myRequest)
+        createWebview()
+        webviewConstraint()
+        loadWebview()
     }
 
     override var representedObject: Any? {
@@ -34,5 +28,40 @@ class ViewController: NSViewController, WKUIDelegate, WKNavigationDelegate {
         // Update the view, if already loaded.
         }
     }
+    
+    // MARK: 自訂函式
+    // 建立 WebView
+    fileprivate func createWebview() {
+        
+        self.webView.uiDelegate = self
+        self.webView.navigationDelegate = self
+        
+        webView.frame = .zero
+        self.view.addSubview(webView)
+    }
+    
+    // 約束 webview
+    fileprivate func webviewConstraint() {
+        webView.translatesAutoresizingMaskIntoConstraints=false
+        
+        let ctLeft = NSLayoutConstraint(item: webView, attribute: .leading, relatedBy: .equal, toItem: self.view, attribute: .leading, multiplier: 1, constant: 20)
+        let ctRight = NSLayoutConstraint(item: webView, attribute: .trailing, relatedBy: .equal, toItem: self.view, attribute: .trailing, multiplier: 1, constant: -20)
+        let ctTop = NSLayoutConstraint(item: webView, attribute: .top, relatedBy: .equal, toItem: self.view, attribute: .top, multiplier: 1, constant: 50)
+        let ctBottom = NSLayoutConstraint(item: webView, attribute: .bottom, relatedBy: .equal, toItem: self.view, attribute: .bottom, multiplier: 1, constant: -20)
+        
+        //self.view.addConstraint(ctLeft)
+        //self.view.addConstraint(ctRight)
+        //self.view.addConstraint(ctTop)
+        //self.view.addConstraint(ctBottom)
+        
+        NSLayoutConstraint.activate([ctLeft,ctRight,ctTop,ctBottom])
+    }
+    // 載入網頁
+    fileprivate func loadWebview() {
+        let myURL=URL(string: "https://www.apple.com")
+        let myRequest = URLRequest(url: myURL!)
+        webView.load(myRequest)
+    }
 }
+
 
